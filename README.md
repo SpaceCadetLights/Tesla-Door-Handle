4e402e9241e22c3411a00fb78f3d62507adc2504
Isaac Norris <56008518+SpaceCadetLights@users.noreply.github.com>
2021-07-01 23:21:43 -0400

    Initial Release
    Opens handle, preliminary rfid controls, needs more work but i'm on it!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tesla Door Handle
  This is the code for controlling a hacked tesla door handle. It runs on an esp32, and converts a tesla door handle into a home door handle that uses RFID to unlock or lock your home's door. 
+
+ The tesla door handle has a 12v dc motor, limit switches, and a lovely pressure sensor so adapting it to control a home door just simply has to be done :) Using this code you can wire up your tesla door handle to an esp32, or any other microcontroller, and operate the handle using an RFID tag reader. Feel free to adapt this code to whatever application you like, I have developed it for the sole purpse of making my RV door waaaaay too futuric! 
---
+++ Tesla_Door_Handle/Tesla_Door_Handle.ino
@@ -0,0 +1,310 @@
+/*
+  Code for controlling Tesla Door Handle
+  motor R opens, Motor L closes, O turns motor off
+*/
+
+//-----RFID Libraries and definitions-----
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 21
+#define RST_PIN 17
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+
+
+//------Pin Definitions-----
+
+//Motor Definitions
+
+const int A1A = 32;//define pin 2 for A1A
+const int A1B = 33;//define pin 3 for A1B
+
+//Limit switch definitions
+
+#define InLimitPin 25
+#define OutLimitPin 26
+#define HandleTriggerPin 27
+
+//Light definitions
+#define handleLEDpin 14  //led that is built into the handle
+
+
+//-----Integer Definitions-----
+
+//limit switch integers
+int InLimitStatus = 0;
+int OutLimitStatus = 0;
+int HandleTriggerStatus = 0; //storing handle pulled switch status
+
+
+//Serial definitons
+char receivedChar;
+boolean newData = false;
+
+
+//motion state definitions
+int currentPosition = 0; //0 means closed 1 means open
+int targetPosition = 0; //the handle position we want it to move to
+
+
+void setup() {
+  Serial.begin(115200);
+
+  //Motor Pin Setup
+  pinMode(A1A, OUTPUT);
+  pinMode(A1B, OUTPUT);
+
+  //Limit pin setup
+  pinMode(InLimitPin, INPUT);
+  pinMode(OutLimitPin, INPUT);
+  pinMode(HandleTriggerPin, INPUT);
+
+  //led pin setup
+  pinMode(handleLEDpin, OUTPUT);
+
+  delay(1000);
+
+  //Initializing the handle
+  UpdateLimits();
+  UpdatePosition();
+  if (currentPosition == 0) {
+    Serial.println("Handle Closed");
+  }
+  if (currentPosition == 1) {
+    Serial.println("Handle Opened");
+  }
+
+  //RFID setup
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Listening for RFID cards ...");
+  Serial.println();
+  //If you set Antenna Gain to Max it will increase reading distance
+  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+
+  //Finishing up setup
+  Serial.println("Setup and ready");
+}
+
+void loop() {
+
+  //-----Handle Loop-----
+  UpdateLimits();
+  //debugSwitches();
+  HandleSerial(); //listen for commands for position control
+
+
+  //Listen for position change commands
+  if (targetPosition == 1 && currentPosition == 0) {
+    openHandle();
+  }
+
+  if (targetPosition == 0 && currentPosition == 1) {
+    closeHandle();
+  }
+
+  //Listen for handle pull commands
+  if (HandleTriggerStatus == 1) {
+    HandlePulled(); //runs whatever function needed when handle is pulled
+  } else {
+    HandleNotPulled();
+  }
+
+  //run functions based on handle position
+  if (currentPosition == 1) {
+    HandleOpen();
+  }
+  if (currentPosition == 0) {
+    HandleClosed();
+  }
+
+
+  //-----RFID Loop-----
+  ListenForRFID();
+
+
+}//-----end loop-----:)
+
+//------------Handle State Functions--------------
+
+void HandlePulled() {
+  Serial.println("Handle Pulled");
+}
+
+void HandleNotPulled() {
+
+}
+
+void HandleOpen() {
+  digitalWrite(handleLEDpin, HIGH);
+}
+
+void HandleClosed() {
+  digitalWrite(handleLEDpin, LOW);
+}
+
+//------------motor control functions--------------
+
+void openHandle() {
+  if (OutLimitStatus == 0) {
+    Serial.println("Handle Opening");
+    motor('R');// Turn motor on in opening direction
+  }
+
+  if (OutLimitStatus == 1) {
+    motor('O');// Turn motor OFF
+    Serial.println("Handle Open");
+    currentPosition = 1;  //update current position
+  }
+}
+
+
+void closeHandle() {
+  if (InLimitStatus == 0) {
+    Serial.println("Handle Closing");
+    motor('L');// Turn motor on in opening direction
+  }
+
+  if (InLimitStatus == 1) {
+    motor('O');// Turn motor OFF
+    Serial.println("Handle Closed");
+    currentPosition = 0;  //update current position
+  }
+}
+
+void motor(char d)
+{
+  if (d == 'R') {
+    digitalWrite(A1A, LOW);
+    digitalWrite(A1B, HIGH);
+  } else if (d == 'L') {
+    digitalWrite(A1A, HIGH);
+    digitalWrite(A1B, LOW);
+  } else {
+    //Robojax.com L9110 Motor Tutorial
+    // Turn motor OFF
+    digitalWrite(A1A, LOW);
+    digitalWrite(A1B, LOW);
+  }
+}// motorA end
+
+
+
+//----------position and limit reading functions-------------
+
+void UpdatePosition() {
+
+  if (OutLimitStatus == 1) {
+    currentPosition = 1;  //update current position
+  }
+  if (InLimitStatus == 1) {
+    currentPosition = 0;  //update current position
+  }
+}
+
+void UpdateLimits() {
+  //read limit switches
+  InLimitStatus = digitalRead(InLimitPin);
+  OutLimitStatus = digitalRead(OutLimitPin);
+  HandleTriggerStatus = digitalRead(HandleTriggerPin);
+}
+
+
+void debugSwitches() {
+  Serial.print("In Limit- ");
+  Serial.print(InLimitStatus);
+  Serial.print("---");
+  Serial.print("Out Limit- ");
+  Serial.print(OutLimitStatus);
+  Serial.print("---");
+  Serial.print("Handle Switch- ");
+  Serial.println(HandleTriggerStatus);
+}
+
+
+
+//------------Serial control functions------------
+
+
+void HandleSerial() {
+  if (Serial.available() > 0) {
+    receivedChar = Serial.read();
+
+    if (receivedChar == 'o') {
+      targetPosition = 1;
+    }
+    if (receivedChar == 'c') {
+      targetPosition = 0;
+    }
+    newData = true;
+  }
+}
+
+
+//-----------------RFID Control Functions----------------
+
+void ListenForRFID() {
+
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "59 7D 80 C2") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    targetPosition = 1;  //Open the handle
+    //delay(3000);
+  }
+
+  else   {
+    Serial.println(" Access denied");
+    targetPosition = 0; //Close the handle
+    //delay(3000);
+  }
+}
+
+
+
+
+/**
+   Helper routine to dump a byte array as hex values to Serial.
+*/
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+/**
+   Helper routine to dump a byte array as dec values to Serial.
+*/
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
+}
